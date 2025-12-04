1bbb04b0b4572f26bdb681cd34e68abc72524b47
indomitableSwan <5496520+indomitableSwan@users.noreply.github.com>
2025-12-04 12:56:08 -0500

    config file added with session lenghts and light levels

---
+++ cpblue/config.py
@@ -0,0 +1,14 @@
+lengths = {
+    "focus": 20*60,
+    "short_break": 7*60,
+    "long_break": 15*60,
+    }
+
+env = {
+    # light levels: Voltage across Resistor
+    "dark": .5,
+    "bright": 2,
+    "blinding": 3
+    }
+
+
